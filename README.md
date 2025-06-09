b3c40cf843512188ca39c453393f30294e6aade6
destroyercreator <david@sixpenny.ca>
2025-06-09 11:33:41 -0400

    Add basic whistle timer code

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# lunchwhistle
-a project to implement a break timer which consists of a wemos d1 mini, and a solenoid to sound a train whistle on a pre-programmed series of times
+# Lunch Whistle
+
+This project implements an automated break timer using a WeMos D1 mini (ESP8266) and a relay to trigger a train whistle. A small web interface allows configuration of the whistle times.
+
+## Building
+Use the Arduino IDE or PlatformIO with the ESP8266 core. Copy `src/main.cpp` into your sketch and provide your WiFi credentials.
+
+## Usage
+1. Flash the code to the WeMos D1 mini.
+2. Connect the D1 mini to your WiFi network.
+3. Browse to the device's IP address. The web page lets you enter comma separated times in `HH:MM` format.
+4. At each configured time the relay will activate for one second, sounding the whistle.
---
+++ src/main.cpp
@@ -0,0 +1,122 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ArduinoJson.h>
+#include <time.h>
+
+const char* ssid = "your_ssid";     // replace with your WiFi SSID
+const char* password = "your_password"; // replace with your WiFi password
+
+const int relayPin = D1; // Relay control pin
+const int MAX_TIMES = 8; // Max number of whistle times
+
+struct WhistleTime {
+  int hour;
+  int minute;
+};
+
+WhistleTime whistleTimes[MAX_TIMES];
+int numTimes = 0;
+
+ESP8266WebServer server(80);
+
+void handleRoot();
+void handleConfig();
+void checkWhistle();
+
+void setup() {
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print('.');
+  }
+  Serial.println(" connected");
+
+  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+  Serial.println("Waiting for NTP time sync");
+  time_t now = time(nullptr);
+  while (now < 8 * 3600 * 2) {
+    delay(500);
+    Serial.print('.');
+    now = time(nullptr);
+  }
+  Serial.println(" time synced");
+
+  server.on("/", HTTP_GET, handleRoot);
+  server.on("/config", HTTP_POST, handleConfig);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+  checkWhistle();
+}
+
+void handleRoot() {
+  String page = "<html><body><h1>Lunch Whistle Timer</h1>";
+  page += "<form method='POST' action='/config'>";
+  page += "Times (HH:MM, comma separated):<br/>";
+  page += "<input type='text' name='times'/>";
+  page += "<input type='submit' value='Set'/></form>";
+  page += "<h2>Current Times</h2><ul>";
+  for (int i=0;i<numTimes;i++) {
+    page += "<li>" + String(whistleTimes[i].hour) + ":" + (whistleTimes[i].minute<10?"0":"") + String(whistleTimes[i].minute) + "</li>";
+  }
+  page += "</ul></body></html>";
+  server.send(200, "text/html", page);
+}
+
+void handleConfig() {
+  if (!server.hasArg("times")) {
+    server.send(400, "text/plain", "Missing times parameter");
+    return;
+  }
+  String timesArg = server.arg("times");
+  numTimes = 0;
+  int last = 0;
+  while (last < timesArg.length() && numTimes < MAX_TIMES) {
+    int comma = timesArg.indexOf(',', last);
+    if (comma == -1) comma = timesArg.length();
+    String pair = timesArg.substring(last, comma);
+    int colon = pair.indexOf(':');
+    if (colon > 0) {
+      int h = pair.substring(0, colon).toInt();
+      int m = pair.substring(colon + 1).toInt();
+      if (h >= 0 && h < 24 && m >= 0 && m < 60) {
+        whistleTimes[numTimes].hour = h;
+        whistleTimes[numTimes].minute = m;
+        numTimes++;
+      }
+    }
+    last = comma + 1;
+  }
+  server.sendHeader("Location", "/");
+  server.send(303);
+}
+
+void checkWhistle() {
+  static time_t lastCheck = 0;
+  time_t now = time(nullptr);
+  if (now == lastCheck) return;
+  lastCheck = now;
+
+  struct tm* timeinfo = localtime(&now);
+  int currentHour = timeinfo->tm_hour;
+  int currentMinute = timeinfo->tm_min;
+  int currentSecond = timeinfo->tm_sec;
+
+  for (int i=0; i<numTimes; i++) {
+    if (whistleTimes[i].hour == currentHour && whistleTimes[i].minute == currentMinute && currentSecond == 0) {
+      digitalWrite(relayPin, HIGH); // activate relay
+      delay(1000); // 1 second whistle
+      digitalWrite(relayPin, LOW);
+    }
+  }
+}
+
